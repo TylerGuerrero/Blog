5f1ddfaa4827fdad15491aa864cd2f5b2778d05d
Tyler <guerrero.tyler3@gmail.com>
2021-04-23 19:59:25 -0400

    Added Components

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <a href='/'>Home </a>
+            <a href='/create'>New Blog</a>
+        </nav>
+     );
+}
+ 
+export default NavBar;  
